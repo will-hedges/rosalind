00d843778ea9e8e9cf8386c52cfdf58493e206e0
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2020-12-26 21:25:02 -0600

    solved PPER, started PROB

---
+++ pper.py
@@ -0,0 +1,14 @@
+import math
+import os
+from pathlib import Path
+
+
+os.chdir(Path(__file__).parent)
+
+
+def pper(n, k):
+    return math.factorial(n) // math.factorial(n - k) % 1000000
+
+
+print(pper(21, 7)) # test
+print(pper(90, 10))
---
+++ prob.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+
+os.chdir(Path(__file__).parent)
+
+
+with open('prob_test.txt', 'r') as infile:
+    pass
+
