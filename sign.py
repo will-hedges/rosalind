e56a0f54f4c2d1d94ff6e664986289713527d4c4
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2020-12-27 12:21:53 -0600

    started SIGN
    * sign.py works for test case

---
+++ sign.py
@@ -0,0 +1,40 @@
+#! python3
+# sign.py - http://rosalind.info/problems/sign/
+
+
+import itertools
+import logging
+import os
+from pathlib import Path
+
+
+logging.basicConfig(level=logging.DEBUG, format=' %(levelname)s - %(message)s')
+# logging.disable(logging.CRITICAL)
+
+os.chdir(Path(__file__).parent)
+
+
+def sign(n):
+
+    lst = []
+    for i in range(1, n+1):
+        lst.append(i)
+        lst.append(-i)
+
+    perms = [perm for perm in itertools.permutations(lst, 2)]
+    for perm in perms:
+        a, b = perm[0], perm[1]
+        if abs(a) == abs(b):
+            perms.remove(perm)
+
+    with open('sign_ans.txt', 'w') as outfile:
+        outfile.write(f'{len(perms)}\n')
+        for perm in perms:
+            perm = map(str, perm)
+            outfile.write(f'{" ".join(perm)}\n')
+
+    print('Done.')
+    return
+
+
+sign(2)
