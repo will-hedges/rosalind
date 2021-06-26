4e6c8632cc54d0927ecd5e3d4a659a9cda3d03f7
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2021-06-26 14:40:15 -0500

    solved RSTR

--- rstr.py
+++ rstr.py
@@ -3,16 +3,36 @@
 
 
 def prst(N, x, s):
-    pass
+    p = 1               # initialize overall probability of any string
+    p_cg = x / 2        # probability of either G or C (GC content)
+    p_at = (1 - x) / 2  # probability of either A or T (complement of GC)
+
+    base_dict = {base: s.count(base) for base in 'ATGC'}
+
+    # determine the probability p of the string s forming for one string
+    #   the exponent is basically 'out of all the CG/AT in this string
+    #       this has to be one of them'
+    for base, count in base_dict.items():
+        if base in 'GC':
+            p *= p_cg**count
+        else:
+            p *= p_at**count
+
+    # the probability t 'not s' that there is no random string s in N strings
+    t = (1 - p) ** N
+    # the probability u 'not t', that a random string == s out of N strings
+    u = 1 - t
+
+    return "{:0.3f}".format(u)
 
 
 def main():
     with open('rosalind_rstr.txt') as file:
-        N, x = file.readline().split()
-        N, x = int(N), float(x)
-        s = file.readline().strip()
-
-    prst(N, x, s)
+        N, x, s = file.read().strip().split()
+        N = int(N)
+        x = float(x)
+    ans = prst(N, x, s)
+    print(ans)
 
 
 if __name__ == '__main__':
